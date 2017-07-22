29b2fbd19f67a00b9ad475960fe78cf62167e0d4
Elise Wunderlich <elisewunderlich@Elises-MacBook.local>
2017-07-22 13:01:56 -0600

    First Commit

---
+++ src/SayHello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elisewunderlich
+ * Date: 7/22/17
+ * Time: 12:45 PM
+ */
+
+namespace HelloWorld;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
